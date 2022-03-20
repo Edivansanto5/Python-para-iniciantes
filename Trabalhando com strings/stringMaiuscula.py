b0ac8493914f12d7ce240ccaf92a9206fb877d37
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-19 23:00:40 -0300

    finalizando operação com string

---
+++ Trabalhando com strings/stringMaiuscula.py
@@ -0,0 +1,7 @@
+#colocando as palavras em maisculas 
+
+nome = 'edivan dos santos de alencar'
+print(nome.upper())
+
+nome2 = 'hello world'
+print(nome2.upper())
---
+++ Trabalhando com strings/stringMinuscula.py
@@ -0,0 +1,8 @@
+#transformando a frase em maiscula
+nome = 'maria de fatima leal'
+print(nome.upper())
+
+#Agora vamos trasformar a frase em novamente em minusculas
+print(nome.capitalize())
+
+print (nome.casefold())
