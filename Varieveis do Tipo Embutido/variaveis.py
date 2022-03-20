63aa626c315f73f9db04d8e8585be533f38429fa
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-19 22:01:58 -0300

    finalizando variaveis

---
+++ Varieveis do Tipo Embutido/variaveis.py
@@ -0,0 +1,21 @@
+#existem 3 principais variaveis na linguagem python
+# a primeira str ou string
+# para alocar uma variavel na memoria e devido acresentar o sinal de = após o nome da #varivel.
+
+nome = 'marcos'
+print(nome)
+print(type(nome))
+print('')
+
+# variavel do tipo inteiro 
+numero = 45
+print(numero)
+print(type(numero))
+print('')
+
+# variavel do tipo float ou dupla precisão
+numeroDecimal  = 3.14159
+print(numeroDecimal)
+print(type(numeroDecimal))
+print('')
+
