5a5acfc56ae3bc15bcece5de683241ec36c6b9fe
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-19 22:39:36 -0300

    finalizando a parte do resto da divisão

--- Operadores Ariticometrico/operadores.py
+++ Operadores Ariticometrico/operadores.py
@@ -19,8 +19,25 @@ numero6 = 10
 multiplicado = numero5 * numero6
 print(multiplicado)
 
-#Operador de divisão
+#Operador de divisão Inteira com uma casa
 numero7 = 20
 numero8 = 4
 divisao = numero7 / numero8
 print(divisao)
+
+#operador divisão inteira
+numero9 = 20
+numer10 = 4
+restoDaDivisao = numero9 // numer10
+print(restoDaDivisao)
+
+num = 20
+num1 = 4
+restoDivisao = num % num1
+print(restoDivisao)
+
+#operadores de expociação
+#elevar um valor ao quadrado ou terceira potencia ou mais 
+valor1 = 5**2
+
+print(valor1)
