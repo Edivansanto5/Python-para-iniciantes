353296121fe3d88f5a2a6f49c3dcaec2314c8720
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-20 22:02:25 -0300

    finalizando a atividade04

--- Exercicio de Introdução/Atividade04.py
+++ Exercicio de Introdução/Atividade04.py
@@ -1,3 +1,7 @@
 '''
     Com	o conteúdo aprendido até aqui já é possível	começar	a escrever o primeiro script.Crie um arquivo programa.py e faça um	código	que	vai	pedir que o	usuário	entre com	dois valores e,	em seguida,	o programa	deve imprimir este valor.
 '''
+valor1 = int(input('Digite um numero: '))
+valor2 = int(input('Digite outro nome: '))
+print(valor1)
+print(valor2)
