d7e223498ea59d69672ddbf1caddff7655b80bba
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-20 21:52:44 -0300

    finalizando a aula de entrada de dados do tipo float

---
+++ Entrada do teclado/entradaFloat.py
@@ -0,0 +1,14 @@
+# aqui vamos aprender de como pegar dados do teclado do tipo float ou tambem conhecido como dupla precisao ou double
+# nesse programa especifico vamos fazer um programa pra ler 4  nota de um aluno, e somar e tirar a media.
+
+nota1 = float(input('informe sua primeira nota: ' ))
+nota2 = float(input('Informe a segunda nota: '))
+nota3 = float(input('Informe a terceira nota: '))
+nota4 = float(input('Informe a ulima nota: '))
+somaDasNotas = nota1 + nota2 + nota3 + nota4
+mediaDaNotas = somaDasNotas / 4
+print(mediaDaNotas)
+
+
+    
+
