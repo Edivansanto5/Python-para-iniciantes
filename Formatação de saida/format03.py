477e8ab53d4b9ff6dc0c82bfd1e5325f3719c742
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-21 20:34:08 -0300

    finalizando assunto de formatação com format

---
+++ Formatação de saida/format03.py
@@ -0,0 +1,7 @@
+'''
+    Há outras funções de formatação	e a	format() é a principal delas e a mais utilizada.Podemos passar qualquer tipo de	parâmetro e	ela	é especialmente	útil para formatar números passando seu format code. Por exemplo, podemos arredondar o número flutuante	 245.2346 para duas	 casas decimais	através	do	código	de	formatação :.2f
+
+'''
+pi = 3.14159598678
+print('A redondando para {:.2f}'.format(pi))
+print('O valor original era: {}'.format(pi))
