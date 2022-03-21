e0fe6542c2fe7755ef32448753ce7801054f866f
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-20 21:37:30 -0300

    finalizando a aula de entrada de dados do tipo string

---
+++ Entrada do teclado/entradaString.py
@@ -0,0 +1,8 @@
+#aqui vamos ver a entrada do tipo string 
+nome = str(input('Informe seu nome: '))
+informacao = str(input('Informe o nome de dua mae: '))
+informacao2 = str(input('Informe o nome de dua pai: '))
+print()
+print('Seu nome e ',nome,'e seus pais se chama ')
+print(informacao, ' e')
+print(informacao2)
