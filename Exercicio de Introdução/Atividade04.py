985d3203fb801ee9d724779ddd451004703f1188
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-20 21:59:12 -0300

    criando a atividade04

---
+++ Exercicio de Introdução/Atividade04.py
@@ -0,0 +1,3 @@
+'''
+    Com	o conteúdo aprendido até aqui já é possível	começar	a escrever o primeiro script.Crie um arquivo programa.py e faça um	código	que	vai	pedir que o	usuário	entre com	dois valores e,	em seguida,	o programa	deve imprimir este valor.
+'''
