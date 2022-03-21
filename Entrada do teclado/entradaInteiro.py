4a3dae4eab76b87b91d8ebaa566b4878721d7da2
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-20 21:26:33 -0300

    finalizando aula de entrada de inteiros

---
+++ Entrada do teclado/entradaInteiro.py
@@ -0,0 +1,5 @@
+# aqui vamos mosrtrar como se escreve um progrma pra pedir dados como entrada
+numero1 = int(input('Digite uma numero:' ))
+numero2 = int(input('Digite outro numero:' ))
+soma = numero1 + numero2
+print('A soma vale ',soma)
