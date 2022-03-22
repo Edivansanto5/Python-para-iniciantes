ee62e04a246cc5d222e1c423ac88a8ab01ff8423
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-21 21:26:57 -0300

    finalizando atividade de jogo

--- Exercicio de Introdução/Aividade06.py
+++ Exercicio de Introdução/Aividade06.py
@@ -2,3 +2,11 @@
     JOGO DA ADVINHAÇÃO
 '''
 tentaiva = int(input('Digite um numero de [ 0 a 10 ] para achar o igual: '))
+numeroCerto = 5
+
+if(tentaiva == 5):{
+    print('Parabéns você acertou!!!!! {}'.format(numeroCerto))
+}
+else:
+    print('Errou!!!!')
+print('Fim de Jogo')
