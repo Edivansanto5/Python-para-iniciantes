c095ee1829588090be0c26f1f2a6b706dd5586ac
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-22 20:26:19 -0300

    colocando o comando break para parar o jogo

--- Exercicio de Introdução/AdvinhaComNumeroDeChances.py
+++ Exercicio de Introdução/AdvinhaComNumeroDeChances.py
@@ -1,3 +1,24 @@
 '''
     Vamos atualizar o nosso jogo e da um numero de chance para o nosso jogador!
 '''
+numeroSecreto = 15
+totalDeTentivas = 5
+while(totalDeTentivas > 0): 
+    palpite = int(input('Digite um numero para saber se vc sabe o numero Secreto: '))
+    print('Voce Digitou ',palpite)
+    
+    #comparação entre os valores
+    acertou = palpite == numeroSecreto
+    maior = palpite > numeroSecreto
+    menor = palpite < numeroSecreto
+    
+    if(acertou):
+        print('Acertou mizeravel ')
+        break
+    elif(maior):
+        print('Seu chute está ACIMA do numero segreto ')
+    elif(menor):
+        print('Seu chute está ABAIXO do numero segreto ')
+    totalDeTentivas = totalDeTentivas - 1
+print('Fim de jogo')
+    
