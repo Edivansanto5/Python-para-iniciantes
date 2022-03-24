d0dbb2e77d306f9c79eedbd1ffe3b72602601249
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-23 23:44:37 -0300

    atualizando jogo

--- Estrutura de dados/jogoDaForca.py
+++ Estrutura de dados/jogoDaForca.py
@@ -4,12 +4,8 @@ print('************************************')
 palavraSecreta = 'banana'
 acertou = False
 enforcou = False
-while(not acertou and not enforcou):
-    chute = input('Digite uma letra')
-    posicao = 0
-    for letra in palavraSecreta:
-        if(chute == letra):
-            print('Achei a letra {} na posisao {}'.format(letra,index))
-        posicao = posicao - 1
-        print('continua')
+errou = False
+
+while(not acertou and not errou):
+    chute = input('Digite uma lentra ')
     
