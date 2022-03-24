1c07edbf35e4fc1012ff730dfbde91fde3acc821
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-23 23:45:38 -0300

    deu merda aqui

--- Estrutura de dados/jogoDaForca.py
+++ Estrutura de dados/jogoDaForca.py
@@ -2,10 +2,4 @@ print("************************************")
 print('*****Bem vindo ao Jogo da Forca*****')
 print('************************************')
 palavraSecreta = 'banana'
-acertou = False
-enforcou = False
-errou = False
 
-while(not acertou and not errou):
-    chute = input('Digite uma lentra ')
-    
