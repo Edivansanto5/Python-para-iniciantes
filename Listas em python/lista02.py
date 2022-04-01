e5ec1b0b2eb3e0f61fe7eb1c3aac442feca5e6be
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-31 22:01:13 -0300

    finalizando aula de lista dos meses

--- Listas em python/lista02.py
+++ Listas em python/lista02.py
@@ -1,2 +1,17 @@
 meses = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
 
+print(meses)
+print()
+
+print(meses[11])
+print(meses[10])
+print(meses[9])
+print(meses[8])
+print(meses[7])
+print(meses[6])
+print(meses[5])
+print(meses[4])
+print(meses[3])
+print(meses[2])
+print(meses[1])
+print(meses[0])
