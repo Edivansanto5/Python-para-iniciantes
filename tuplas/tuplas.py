64eb4013921b50cac48487c627270035c81f8c11
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-02 14:38:22 -0300

    finalizando aula de tuplas

--- tuplas/tuplas.py
+++ tuplas/tuplas.py
@@ -1,6 +1,11 @@
 nomes = ('Mario','Fernando','Fabiana','Francisca')
 diasSemanas = ('Domingo','Segunda','terça','Quarta','Quinta','Sexta','Sábado')
 
+# ou então podemos omitir os parentes e so colocar os elementos separados por , 
+
+cidades = 'Água branca','Piripiri','Santa Cruz','Esta Zero'
+
 print(nomes)
 print(diasSemanas)
 print(type(nomes))
+print(cidades)
