7ab8e453a5e938c6fe1ccfe719e363754ceb5dc4
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-02 14:03:55 -0300

    adcionado elemen na lis com a funçao exende

---
+++ Listas em python/listaExtende.py
@@ -0,0 +1,14 @@
+nomes = []
+paises = []
+cidades = []
+
+# Agora vamos adicionar elen nas lis com a função extend
+nomes.extend(['silvana','Paulo','Mariana'])
+paises.extend(['Corea','Brasil','Chile','Perú'])
+cidades.extend(['Picos','Valença','Pio IX','Passagem franca do piauí'])
+
+print(nomes)
+print(paises)
+print(cidades)
+
+
