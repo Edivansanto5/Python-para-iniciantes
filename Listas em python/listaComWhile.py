6cd789be33d04e87845b8bbc22eac1735d0876f7
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-02 11:11:11 -0300

    testando um comite

--- Listas em python/listaComWhile.py
+++ Listas em python/listaComWhile.py
@@ -2,7 +2,7 @@ meses = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto',
 
 numero = 1
 
-while (numero < 4):
+while (numero > 4):
     mes = input('Digite um mês ( 1 - 12): ')
     if 1 <= mes <= 12:
         print('O mes é {}'.format(mes[mes - 1 ]))
