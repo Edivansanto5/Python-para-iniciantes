6683d03eeadd8ba9722ee92f0842668ab41be919
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-02 14:30:50 -0300

    finalizando aula de tuplas

---
+++ tuplas/tuplas.py
@@ -0,0 +1,6 @@
+nomes = ('Mario','Fernando','Fabiana','Francisca')
+diasSemanas = ('Domingo','Segunda','terça','Quarta','Quinta','Sexta','Sábado')
+
+print(nomes)
+print(diasSemanas)
+print(type(nomes))
