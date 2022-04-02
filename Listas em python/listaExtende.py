66064e719dbd63694912149fdb29c9aa71afc4a3
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-02 14:08:42 -0300

    duplicando lis de nomes

--- Listas em python/listaExtende.py
+++ Listas em python/listaExtende.py
@@ -7,8 +7,13 @@ nomes.extend(['silvana','Paulo','Mariana'])
 paises.extend(['Corea','Brasil','Chile','Perú'])
 cidades.extend(['Picos','Valença','Pio IX','Passagem franca do piauí'])
 
+# duplicando lis
+lisMul = nomes*2
+
 print(nomes)
 print(paises)
 print(cidades)
+
+print(lisMul)
 
 
