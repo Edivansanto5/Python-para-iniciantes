cebfde90edc01af1367ff8ce7dd00ccdbdc4f465
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-02 14:49:31 -0300

    finalizando aulas de tupla

---
+++ tuplas/funcaotupla.py
@@ -0,0 +1,16 @@
+
+from unicodedata import numeric
+
+
+texto = 'Macacos do cerrado'
+tuple(texto)
+
+numero = (1,2,3,4,5,6,7,8,9)
+tuple(numero)
+print()
+print(texto)
+print(tuple(texto))
+print()
+
+print(numero)
+print(tuple(numero))
