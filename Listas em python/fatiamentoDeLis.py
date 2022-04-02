d88e5b465ea5764f8b88a0a751cc57487a5361fa
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-02 11:41:23 -0300

    pegando todos os elementos da lista

--- Listas em python/fatiamentoDeLis.py
+++ Listas em python/fatiamentoDeLis.py
@@ -1,2 +1,5 @@
-lista = [1,2,3,4,5]
-print(lista[1:5])
+lista = [0,1,2,3,4,5]
+print(lista[1:5])# vai começar do indice 1 até 5
+print(lista[:4]) # vai começar do indice 0 até 5 0u seja todos os elementos
+ 
+
