da221dc73420b91643b44627c853f6d758ca1d5a
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-03 15:45:01 -0300

    fazendo um conjunto de uma lista e como sabemos ela nao permite nomes duplicados

---
+++ CONJUNTOS/CONJUNTOS.py
@@ -0,0 +1,8 @@
+'''
+    O	 Python	 também	 inclui	 um	 tipo	 de	 dados	 para	 conjuntos.	 Um	 conjunto,	 diferente	 de	 uma
+    sequência, é uma coleção não ordenada e	que	não	admite elementos duplicados.
+'''
+list = {'laranjas','macacos','leão','camelo','sabão de coco','laranjas','camelo'}
+list
+print(list)
+#
