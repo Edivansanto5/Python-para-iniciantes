a0bb7f6dea294a61cee56fc04d30151646172f3d
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-03 15:58:26 -0300

    fazendo uma verificação de quantas letras tem na frase sem repetir

--- CONJUNTOS/conju.py
+++ CONJUNTOS/conju.py
@@ -2,5 +2,5 @@
 Usos básicos incluem testes	 de	 associação	 e	 eliminação	 de	 entradas	 duplicadas.Os objetos	 de conjunto também	 suportam	 operações	 matemáticas como	 união,	 interseção,diferença e	diferença simétrica.Podemos	transformar	um	texto	em	um	conjunto	com	a	frunção	set() e	testar	os	operações:
 
 '''
-a = set('Abacaxi')
+a = set('abacaxi de macaco de solada apera')
 print(a)
