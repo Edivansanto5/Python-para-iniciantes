da126e3f44784945264035d4b80a4e3a41166a01
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-03 16:01:52 -0300

    fazendo operações matematicas com as funçoes

--- CONJUNTOS/conju.py
+++ CONJUNTOS/conju.py
@@ -2,5 +2,11 @@
 Usos básicos incluem testes	 de	 associação	 e	 eliminação	 de	 entradas	 duplicadas.Os objetos	 de conjunto também	 suportam	 operações	 matemáticas como	 união,	 interseção,diferença e	diferença simétrica.Podemos	transformar	um	texto	em	um	conjunto	com	a	frunção	set() e	testar	os	operações:
 
 '''
-a = set('abacaxi de macaco de solada apera')
+a = set('abacaxi')
+b = set('macaco')
+
 print(a)
+print(b)
+# fazendo a diferença 
+resul = a-b
+print(resul)
