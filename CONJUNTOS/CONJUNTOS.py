b193b550a0117fa5d7f15527a3ef9d1ba8fdcc40
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-03 15:48:40 -0300

    fazendo uma comparaçao entre uma funçao do tipo conjunto com uma lista normal

--- CONJUNTOS/CONJUNTOS.py
+++ CONJUNTOS/CONJUNTOS.py
@@ -2,7 +2,15 @@
     O	 Python	 também	 inclui	 um	 tipo	 de	 dados	 para	 conjuntos.	 Um	 conjunto,	 diferente	 de	 uma
     sequência, é uma coleção não ordenada e	que	não	admite elementos duplicados.
 '''
+from __future__ import print_function
+
+
 list = {'laranjas','macacos','leão','camelo','sabão de coco','laranjas','camelo'}
 list
 print(list)
-#
+print(type(list))
+
+# vamos comparar com uma lista normal
+lista = ['laranjas','macacos','leão','camelo','sabão de coco','laranjas','camelo']
+print(lista)
+print(type(lista))
