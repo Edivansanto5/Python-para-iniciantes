5b746f297d330029ddf0eac39ecf3769deed32d7
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-03 15:55:28 -0300

    fazendo uma verificaçao se uma determinada lertra está presente em uma frase e se tiver ela vai ser inginorada

---
+++ CONJUNTOS/conju.py
@@ -0,0 +1,6 @@
+'''
+Usos básicos incluem testes	 de	 associação	 e	 eliminação	 de	 entradas	 duplicadas.Os objetos	 de conjunto também	 suportam	 operações	 matemáticas como	 união,	 interseção,diferença e	diferença simétrica.Podemos	transformar	um	texto	em	um	conjunto	com	a	frunção	set() e	testar	os	operações:
+
+'''
+a = set('Abacaxi')
+print(a)
