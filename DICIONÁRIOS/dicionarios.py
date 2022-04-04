3725b7da2a3b8392604e9798100e95d638767e37
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-03 21:25:38 -0300

    acessando os elemen pelo atributo

---
+++ DICIONÁRIOS/dicionarios.py
@@ -0,0 +1,9 @@
+'''
+Qualquer	 chave	 de	 um	 dicionário	 é	 associada	 (ou	 mapeada)	 a	 um	 valor.	 Os	 valores	 podem	 ser
+qualquer	tipo	de	dado	do	Python.	Portanto,	os	dicionários	são	pares	de	chave-valor	não	ordenados.
+'''
+
+nomes = {'nome': 'Maria','idade': 45, 'Sexo': 'Feminina'}
+
+print(nomes['nome'])
+
