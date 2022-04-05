1540254b2c8fd3fb156d6a6b2d78d95b211d50b0
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-05 14:57:44 -0300

    adcionando um elemento no dicionario

--- DICIONÁRIOS/dicionario02.py
+++ DICIONÁRIOS/dicionario02.py
@@ -1,7 +1,9 @@
 pessoa = {'nome':'Marcos ','Idade':23 ,'Cidade':'Picos'}
+pessoa['Pais'] = 'Espanha'
 
 print(pessoa)
-#print(pessoa[0])# vai ocorrer um erro
-#print(pessoa.nome) tambem vai ocorrer um erro
 print(pessoa['nome'])
 print(pessoa['Idade'])
+print('ela possui ',pessoa['Idade'])
+#print(pessoa[0])# vai ocorrer um erro
+#print(pessoa.nome) tambem vai ocorrer um erro
