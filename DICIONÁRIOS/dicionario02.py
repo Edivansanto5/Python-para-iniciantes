e0ecee2daa76b4f84e7195d3b53343fcdb012a82
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-05 14:10:25 -0300

    acessando os atributos pelos nomes

--- DICIONÁRIOS/dicionario02.py
+++ DICIONÁRIOS/dicionario02.py
@@ -1,4 +1,6 @@
 pessoa = {'nome':'Marcos ','Idade':23 ,'Cidade':'Picos'}
 
 print(pessoa)
-print(pessoa[0])
+#print(pessoa[0])# vai ocorrer um erro
+#print(pessoa.nome) tambem vai ocorrer um erro
+print(pessoa['nome'])
