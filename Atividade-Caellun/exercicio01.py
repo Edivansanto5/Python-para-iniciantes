eac8ebe0265f8c829c571b66c3928c0689b3a4bb
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-05 20:49:45 -0300

    criando uma lista

--- Atividade-Caellun/exercicio01.py
+++ Atividade-Caellun/exercicio01.py
@@ -7,4 +7,9 @@
     e)imprimaa média dos elementos
     f)imprimaa soma	dos	elementos de valor negativo
 '''
+from operator import index
+
+
 lista = [12,-2,4,8,29,45,78,36,-17,2,12,8,3,3,-52];
+for lista in range(0 ,15):
+    print(lista)
