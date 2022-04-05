cda50fe7ad25d356e8cb0ab84f903caf6720a378
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-05 15:36:32 -0300

    fazendo um novo dicionerio com a funçao dict

--- DICIONÁRIOS/dicionario02.py
+++ DICIONÁRIOS/dicionario02.py
@@ -1,9 +1,16 @@
 pessoa = {'nome':'Marcos ','Idade':23 ,'Cidade':'Picos'}
 pessoa['Pais'] = 'Espanha'
+pessoa.keys()
 
+print(pessoa.keys())
+print(pessoa.values())
 print(pessoa)
 print(pessoa['nome'])
 print(pessoa['Idade'])
-print('ela possui ',pessoa['Idade'])
+print(pessoa['Idade'])
 #print(pessoa[0])# vai ocorrer um erro
 #print(pessoa.nome) tambem vai ocorrer um erro
+
+a = dict(um=1,dois=2,tres=3,quatro=2+2)#podemos fazer operaçoes dentro de uma criaçao de um dicionario
+
+print(a)
