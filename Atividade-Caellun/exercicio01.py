5f4fd7b68a61e144de5e6459481ba74109bca715
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-05 15:50:26 -0300

    criando a primeira atividade do exercicio proposto

---
+++ Atividade-Caellun/exercicio01.py
@@ -0,0 +1,9 @@
+'''
+    Dada a lista = [12,-2,4,8,29,45,78,36,-17,2,12,8,3,3,-52]faça um programa que:
+    a)imprimao maior elemento
+    b)imprimao menor elemento
+    c)imprimaos	números	pares
+    d)imprimao número de ocorrências do	primeiro elemento da lista
+    e)imprimaa média dos elementos
+    f)imprimaa soma	dos	elementos de valor negativo
+'''
