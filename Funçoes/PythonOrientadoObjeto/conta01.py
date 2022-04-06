99cf98f9988912aea4eecfc900e9e04904175310
Mr-Ramos <rafaelmaggot64@gmail.com>
2022-04-06 13:27:53 -0300

    exibindo todas as contas.

--- Funçoes/PythonOrientadoObjeto/conta01.py
+++ Funçoes/PythonOrientadoObjeto/conta01.py
@@ -4,14 +4,20 @@ saldo = 340
 limite = 5000
 
 # fazendo outra conta
-numero2 = 549625792
-titular2 = ' Mara sousa'
-saldo2 = 124
-limite2 = 5000
+numero1 = 549625792
+titular1 = ' Mara sousa'
+saldo1 = 124
+limite1 = 5000
 
 # fazendo outra conta
 numero2 = 906429343
 titular2 = ' Rosana soares'
 saldo2 = 1240
 limite2 = 5000
+
+# imprimir todas as contas
+print('imprimindo todas as contas')
+print(numero,titular,saldo,limite)
+print(numero1,titular1,saldo1,limite1)
+print(numero2,titular2,saldo2,limite2)
 
