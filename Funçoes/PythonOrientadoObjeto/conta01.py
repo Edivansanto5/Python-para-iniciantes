da11075461e14589adf7ff9409e63da4c53adfd0
Mr-Ramos <rafaelmaggot64@gmail.com>
2022-04-06 13:13:43 -0300

    fazendo as contas

---
+++ Funçoes/PythonOrientadoObjeto/conta01.py
@@ -0,0 +1,17 @@
+numero = 1932034900
+titular = ' Marlesson'
+saldo = 340
+limite = 5000
+
+# fazendo outra conta
+numero2 = 549625792
+titular2 = ' Mara sousa'
+saldo2 = 124
+limite2 = 5000
+
+# fazendo outra conta
+numero2 = 906429343
+titular2 = ' Rosana soares'
+saldo2 = 1240
+limite2 = 5000
+
---
+++ Funçoes/funcao01.py
@@ -0,0 +1,5 @@
+
+def velocidadeMedia(espaco,tempo):
+  velo = espaco / tempo
+  print(velo)
+velocidadeMedia(315.300,2.50)
