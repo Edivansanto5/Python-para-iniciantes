129f1dae40d21ee1528e73b8e46736a6f40595eb
Mr-Ramos <rafaelmaggot64@gmail.com>
2022-04-06 13:53:45 -0300

    fazendo e exibindo a conta01

--- Funçoes/PythonOrientadoObjeto/conta02.py
+++ Funçoes/PythonOrientadoObjeto/conta02.py
@@ -1,9 +1,18 @@
 
 conta = {"numero":1932034900, "titular":'Marlesson', "saldo":340 , "limite":5000}
+conta1 = {"numero1":794543320, "titular1":'Mara', "saldo1":990 , "limite1":5000}
+
 
 print('imprimindo as contas')
-print(conta['numero'])
+
 print(conta['titular'])
+print(conta['numero'])
 print(conta['saldo'])
+
+print('')
+print(conta1['titular1'])
+print(conta1['numero1'])
+print(conta1['saldo1'])
+
 
 
