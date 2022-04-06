c278885a137e37c33cf65a0ac68b487de3e98918
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-05 22:01:00 -0300

    finalizando parte de numeros impares

--- Atividade-Caellun/exercicio01.py
+++ Atividade-Caellun/exercicio01.py
@@ -10,9 +10,24 @@
 # a)imprimao maior elemento
 lista = [12,-2,4,8,29,45,78,36,-17,2,12,8,3,3,-52];
 valorMaximo = max(lista)# função max() com o parametro o node da lista
-print('alor Máximo',valorMaximo)
+print('Valor Máximo',valorMaximo)
 
 #b)imprimao menor elemento
 lista = [12,-2,4,8,29,45,78,36,-17,2,12,8,3,3,-52]
 valorMinino = min(lista)
 print('Valor Minimo',valorMinino)
+print('Números Pares')
+
+#c)imprima os números pares
+for pares in lista:
+    if pares % 2 == 0:
+        print(pares)
+print(' ')
+
+print('Números Impares')
+# imprima os numeros impares
+for impar in (lista):
+    if impar % 2 == 1:
+        print(impar)
+print()
+
