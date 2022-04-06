b7667491eb653d305a85767657f4bbba61872643
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-05 21:13:50 -0300

    percorendo a lista e revelando o maior

--- Listas em python/pecorrendoAlista.py
+++ Listas em python/pecorrendoAlista.py
@@ -1,4 +1,4 @@
-minhaLista = [12,-2,4,8,29,45,78,36,-17,2,12,8,3,3,-52]
-for x in minhaLista:
-    print(x);
-print('fim do percuso')
+lista = [12,-2,4,8,29,45,78,36,-17,2,12,8,3,3,-52]
+maximo = max(lista)
+print('valor maximo {}'.format(maximo))
+
