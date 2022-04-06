2e0321c23e701f458fb14edfb8eaed81ab19b322
Mr-Ramos <rafaelmaggot64@gmail.com>
2022-04-06 13:21:00 -0300

    fazendo uma conta com a funçao de um dicionario

---
+++ Funçoes/PythonOrientadoObjeto/conta02.py
@@ -0,0 +1,2 @@
+
+conta = {'numero':1932034900, 'ttular':'Marlesson', 'saldo':340 , 'limite':5000}
--- Funçoes/funcao02.py
+++
@@ -1,8 +0,0 @@
-def dados(nome, idade=None):
-  print('nome: {}'.format(nome))
-  if(idade is not None):
-    print('idade: {}'.format(idade))
-  else:
-    print('idade: não informada')
-
-dados('marana')
