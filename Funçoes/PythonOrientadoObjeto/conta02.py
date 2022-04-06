626d2e7acf9b0eae852be45457104c81de716634
Mr-Ramos <rafaelmaggot64@gmail.com>
2022-04-06 13:41:18 -0300

    fazendo a conta com um dicionario.

--- Funçoes/PythonOrientadoObjeto/conta02.py
+++ Funçoes/PythonOrientadoObjeto/conta02.py
@@ -1,2 +1,9 @@
 
-conta = {'numero':1932034900, 'ttular':'Marlesson', 'saldo':340 , 'limite':5000}
+conta = {"numero":1932034900, "titular":'Marlesson', "saldo":340 , "limite":5000}
+
+print('imprimindo as contas')
+print(conta['numero'])
+print(conta['titular'])
+print(conta['saldo'])
+
+
