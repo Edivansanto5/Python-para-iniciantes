ad72a96ab04455a97614b915bb55b80ed5b9402e
Mr-Ramos <rafaelmaggot64@gmail.com>
2022-04-06 21:21:07 -0300

    teste

--- Comando IF/ifParte01.py
+++ Comando IF/ifParte01.py
@@ -1,13 +1,7 @@
-nota1 = float(input('informe uma nota: '))
-nota2 = float(input('informe outra nota: '))
+nota1 = float(input('informe uma nota '))
+nota2 = float(input('informe uma nota '))
 media = (nota1 + nota2)/2
-if(media >= 7):{
-    print('parabens vc esta aprovado',media)
+if(media<7):{
+    print('reprovado')
 }
-if(media <= 6  ):{
-    print('vc esta de prova final ',media)
-}
-else:
 
-    print('vc se lascou ',media)
-      
